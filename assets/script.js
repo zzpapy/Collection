f41a19a574041847e82a88bf02998a20e3a561bf
zzpapy <gregory.pace@hotmail.fr>
2022-12-08 11:14:53 +0100

    toto

--- assets/script.js
+++ assets/script.js
@@ -74,3 +74,22 @@
 
 //     }
 // }
+const btn = document.getElementById('color');
+
+btn.addEventListener('click', function onClick(event) {
+   if(document.body.classList.contains('dark')){
+        document.body.classList.remove("dark")
+        event.target.style.backgroundColor = 'black'
+        event.target.style.color = 'white'
+        event.target.textContent ='Dark'
+    } 
+    else{
+        document.body.classList.add("dark") 
+        event.target.style.backgroundColor ='white' 
+        event.target.style.color = 'black'
+        event.target.textContent ='Light'
+    }
+    
+  
+
+});
