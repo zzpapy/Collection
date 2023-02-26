d5a6f6c9249bad04a0367dfd0a956f01eeaa6805
zzpapy <gregory.pace@hotmail.fr>
2023-02-26 11:11:18 +0100

    test

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import './styles/custom.css';
 
 import { Tooltip, Toast, Popover } from './bootstrap';
 
--- assets/script.js
+++ assets/script.js
@@ -8,23 +8,33 @@ function changeColor(coll, color){
 }
 
 const btn = document.getElementById('color');
-
-btn.addEventListener('click', function onClick(event) {
-   if(document.body.classList.contains('dark')){
-        document.body.classList.remove("dark")
-        event.target.style.backgroundColor = 'black'
-        event.target.style.color = 'white'
-        changeColor(document.getElementsByClassName('top'), 'white'); 
-        event.target.textContent ='Dark'
-    } 
-    else{
-        document.body.classList.add("dark") 
-        event.target.style.backgroundColor ='white'
-        changeColor(document.getElementsByClassName('top'), 'black'); 
-        event.target.style.color = 'black'
-        event.target.textContent ='Light'
-    }
+if(btn){
     
-  
+    btn.addEventListener('click', function onClick(event) {
+       if(document.body.classList.contains('dark')){
+            document.body.classList.remove("dark")
+            event.target.style.backgroundColor = 'black'
+            event.target.style.color = 'white'
+            changeColor(document.getElementsByClassName('top'), 'white'); 
+            event.target.textContent ='Dark'
+        } 
+        else{
+            document.body.classList.add("dark") 
+            event.target.style.backgroundColor ='white'
+            changeColor(document.getElementsByClassName('top'), 'black'); 
+            event.target.style.color = 'black'
+            event.target.textContent ='Light'
+        }
+    });
+}
 
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#inputPassword');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
 });
